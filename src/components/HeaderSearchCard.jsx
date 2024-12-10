0e49f9f5de891e53e2c0d96f12f5d228a68f05c5
Amir.Sh <mobilefactory012@gmail.com>
2024-12-10 16:29:38 +0330

    Edit : HeaderSearchCard in home page

--- src/components/HeaderSearchCard.jsx
+++ src/components/HeaderSearchCard.jsx
@@ -1,49 +1,64 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 import { CiSearch } from "react-icons/ci"
-import { Divider } from "@nextui-org/react"
 
 function HeaderSearchCard() {
   const [selected, setSelected] = useState("option_1")
+  const inputRef = useRef()
+
+  const InputWithIcon = () => {
+    inputRef.current.focus()
+  }
 
   const handleToggle = (opttion) => {
     setSelected(opttion)
   }
 
   return (
-    <div className="h-full max-h-[108px] w-full md:w-[750px] max-w-[816px] bg-white rounded-xl mt-[72px]">
-      <div className="flex justify-around text-2xl font-medium mt-[14px]">
-        <div
-          onClick={() => handleToggle("Option_1")}
-          className={`transition duration-200 ease-in-out cursor-pointer text-Gr11 ${
-            selected === "Option_1" && "font-bold"
-          }`}
-        >
-          اجاره
-          {selected === "Option_1" && (
-            <div className="h-[2px] bg-primary w-40"></div>
-          )}
+    <div className="min-w-80 lg:w-[816px] bg-white rounded-xl mt-[72px]">
+      <div className="flex justify-center text-2xl font-medium mt-[14px]">
+        {/* selection 1 */}
+        <div className="flex flex-col items-center w-36 lg:w-96">
+          <h1
+            onClick={() => handleToggle("Option_1")}
+            className={`cursor-pointer mb-1 text-Gr11 font-medium text-sm lg:text-2xl ${
+              selected === "Option_1" && "font-extrabold"
+            }`}>
+            اجاره
+          </h1>
+          <span
+            className={`h-[2px] w-full ${
+              selected === "Option_1"
+                ? "bg-primary transition ease-linear"
+                : "bg-Gr5"
+            }`}></span>
         </div>
 
-        <div
-          onClick={() => handleToggle("Option_2")}
-          className={`transition duration-200 ease-in-out cursor-pointer text-Gr11 ${
-            selected === "Option_2" && "font-bold"
-          }`}
-        >
-          خرید
-          {selected === "Option_2" && (
-            <div className="h-[2px] bg-primary w-40"></div>
-          )}
+        {/* selection 2*/}
+        <div className="flex flex-col items-center w-36 lg:w-96">
+          <h1
+            onClick={() => handleToggle("Option_2")}
+            className={`cursor-pointer mb-1 text-Gr11 font-medium text-sm lg:text-2xl ${
+              selected === "Option_2" && "font-extrabold"
+            }`}>
+            خرید
+          </h1>
+          <span
+            className={`h-[2px] w-full ${
+              selected === "Option_2"
+                ? "bg-primary transition ease-linear"
+                : "bg-Gr5"
+            }`}></span>
         </div>
       </div>
 
-      <div className="my-auto mx-4">
-        <Divider className="max-w-[800px] text-Gr11" />
-      </div>
-      <div className="flex items-center text-Gr11">
-        <CiSearch className="mr-4 w-5 h-5" />
+      <div className="flex items-center font-semibold my-2 lg:my-3">
+        <CiSearch
+          onClick={InputWithIcon}
+          className="cursor-pointer mr-4 w-5 h-5 lg:w-6 lg:h-6 text-Gr10"
+        />
         <input
-          className="pr-2 mb-3 mt-[10px] border-0 outline-none"
+          ref={inputRef}
+          className="w-full pr-2 outline-none lg:text-lg text-Gr10 placeholder-Gr10"
           type="text"
           placeholder="شهر مورد نظر را جستجو کنید"
         />
