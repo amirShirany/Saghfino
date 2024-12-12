b2c40e8046d1f3e0668420ae97063da0b2dbeab5
Amir.Sh <mobilefactory012@gmail.com>
2024-12-12 11:36:37 +0330

    Add : HeaderHomepage component to lazy for bigImg

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,4 +1,6 @@
-import HeaderHomepage from "../components/HeaderHomePage"
+import React, { Suspense } from "react"
+const HeaderHomepage = React.lazy(() => import("../components/HeaderHomePage"))
+// import HeaderHomepage from "../components/HeaderHomePage"
 import HelpHomePage from "../components/HelpHomePage"
 import HelpMelk from "../components/HelpMelk"
 import Moshavere from "../components/Moshavere"
@@ -8,7 +10,9 @@ import Footer from "../components/Footer"
 function Homepage() {
   return (
     <div className="container xl:max-w-screen-2xl">
-      <HeaderHomepage />
+      <Suspense fallback={<div>Loading...</div>}>
+        <HeaderHomepage />
+      </Suspense>
       <HelpHomePage />
       <HelpMelk />
       <Moshavere />
